eb9f35dc84fa91d9033d7a4231aded584072dbd8
This-is-XiaoDeng <xiaodeng@thisisxd.tk>
2022-12-24 11:13:38 +0800

    [update]完成主体部分代码

--- index.js
+++ index.js
@@ -9,59 +9,73 @@ const password = "2538536XD";
 const port = 8080;
 const client = createClient(uin);
 const app = express();
+const group = 701257458;
 
 
 // Server
-app.get("/files/:group_id/:path", async (req, res) => {
+app.get("/*", async (req, res) => {
   client.logger.info(format("GET {}", req.url));
   // 获取信息
-  if (req.params.path == undefined) {
-    let path = "/";
-  } else {
-    let path = req.params.path;
-  }
-  let group = number(req.params.group_id);
-  client.logger.info(format("Getting {}", path));
+  let path = req.url;
   let gfs = client.acquireGfs(group);
-  let file_list = gfs.ls(path, 0);
-  let html = "";
-  // Read formwork
-  fs.readFile("./web/files.html", function(err,data) {
-    if (err) {
-      client.logger.warning("Cannot read ./web/files.html!");
-      res.statusCode = 500;
-      res.send("Server internal error, please check the log");
-    } else {
-      html = data.toString();
+  if (req.query.download == undefined) {
+    let file_list = await gfs.ls(path, 0);
+    // 读取模板
+    let file = fs.readFileSync("./web/index.html");
+    let html = file.toString();
+    // 合成列表
+    let table = "<table><tr><td>Name</td><td>Size</td></tr>"
+    // console.log(file_list);
+    let size, url;
+    for (var i = 0;i < file_list.length;i++) {
+      // console.log(file_list[i]);
+      if (file_list[i]["is_dir"]) {
+        size = "";
+        url = format("{}", file_list[i]["fid"]);
+      } else {
+        size = file_list[i]["size"];
+        url = format("?download={}", file_list[i]["fid"]);
+      }
+      // 添加文件
+      table = table + "<tr><td><a href=\"{url}\">{name}</a></td><td>{size}</td></tr>"
+        .replace("{name}", file_list[i]["name"])
+        .replace("{size}", size)
+        .replace("{url}", url);
     }
-  });
-  // Table
-  let table = "<table><tr><td>Name</td><td>Size</td><td>Uploader</td><td>Download</td></tr>"
-  for (let i = 0;i <= file_list.length;i++) {
-    table = table + "<tr><td>{name}</td><td>{size}</td><td>{uploader}</td><td><a href=\"{url}\">Download</a></td></tr>"
-      .replace("{name}", file_list[i]["name"])
-      .replace("{size}", file_list[i]["size"])
-      .replace("{uploader}", file_list[i]["user_id"])
-      .replace("url",
-        format(
-          "/download/{group}/{}".replace("{group}", group),
-          file_list[i]["fid"]
-        )
-      );
+    table = table + "</table>";
+    // 返回页面
+    html = html.replace("{path}", path).replace("{group}", group).replace("{file_list}", table);
+    client.logger.info(html);
+    res.send(html);
+  } else {
+    let fid = req.query.download;
+    let url = (await gfs.download(fid)).url;
+    let file_status = await gfs.stat(fid)
+    // 读取模板
+    let file = fs.readFileSync("./web/download.html");
+    let html = file.toString();
+    // 替换模板
+    html = html
+      .replace("{file_name}", file_status["name"])
+      .replace("{uploader}", file_status["user_id"])
+      .replace("{size}", file_status["size"])
+      .replace("{create_time}", file_status["create_time"])
+      .replace("{download_time}", file_status["download_times"])
+      .replace("{url}", url)
+      .replace("{back_url}", url.slice(end=url.lastIndexOf("?")));
+    res.send(html);
   }
-  table = table + "</table>"
-  res.send(html.replace("{path}", path).replace("{group}", group).replace("{file_list}", table));
 });
 
-// Online
+// 上线
 client.on("system.online", () => {
   client.logger.info("Logged in!");
-  let server = app.listen(port, function () {
+  app.listen(port, function () {
     client.logger.info(format("Listening 0.0.0.0:{}", port));
   });
 });
 
-// Login
+// 登录
 client.on("system.login.qrcode", function (e) {
   process.stdin.once("data", () => {
     this.login();
